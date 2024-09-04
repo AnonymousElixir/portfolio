51e3aaf1c194116c10317b7538ef9d8d77799862
AnonymousElixir <cassidy@cassidycamp.work>
2024-09-04 13:33:48 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('projectOne').addEventListener('click', function() {
+    alert('Loading Project...');
+    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+});
+
+document.getElementById('projectTwo').addEventListener('click', function() {
+    alert('Loading Project...');
+    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+});
