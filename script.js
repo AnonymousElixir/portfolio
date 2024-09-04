807803480be6d41626339f9aefa9231c7bd84a5b
AnonymousElixir <cassidy@cassidycamp.work>
2024-09-04 13:51:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.getElementById('hackOne').addEventListener('click', function() {
     alert('Running Exploit-1.0...');
     setTimeout(function() {
         document.getElementById('videoContainer').classList.remove('hidden');
+        var video = document.getElementById('rickrollVideo');
+        video.play(); // Start playing the video automatically
     }, 2000); // Delay to mimic a process running
 });
 
@@ -9,5 +11,7 @@ document.getElementById('hackTwo').addEventListener('click', function() {
     alert('Decrypting data...');
     setTimeout(function() {
         document.getElementById('videoContainer').classList.remove('hidden');
+        var video = document.getElementById('rickrollVideo');
+        video.play(); // Start playing the video automatically
     }, 2000); // Delay to build anticipation
 });
